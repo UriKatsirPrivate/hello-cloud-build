408bd9e7ec9c87c78e1bde43a34a2f49151ccec9
UriKatsirPrivate <46541339+UriKatsirPrivate@users.noreply.github.com>
2020-06-17 18:02:53 +0300

    Name change

--- index.js
+++ index.js
@@ -3,6 +3,6 @@
 * @param {Object} req Cloud Function request context.
 * @param {Object} res Cloud Function response context.
 */
-exports.helloGET = (req, res) => {
+exports.helloCloudBuild = (req, res) => {
   res.send('Hello from Cloud Functions and Cloud Source Repositories33');
 };
