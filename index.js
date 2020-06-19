5b11fba779cb2017bfed0cf5d5d5ee0842d80716
UriKatsirPrivate <46541339+UriKatsirPrivate@users.noreply.github.com>
2020-06-19 18:24:36 +0300

    demo

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
  * @param {Object} res Cloud Function response context.
  */
 exports.helloCloudBuild = (req, res) => {
-  let message = req.query.message || req.body.message || 'Hello Uri Again and Again!';
+  let message = req.query.message || req.body.message || 'Hello, this is a Demo Website';
   res.status(200).send(message);
 };
