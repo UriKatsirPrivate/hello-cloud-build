39ec04c1559883b656eec8d0940f26be289d3b33
Uri Katsir <46541339+UriKatsirPrivate@users.noreply.github.com>
2022-11-20 23:25:54 +0200

    Gen 2 code

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-/* HTTP Cloud Function.
- *
- * @param {Object} req Cloud Function request context.
- * @param {Object} res Cloud Function response context.
- */
-exports.helloCloudBuild = (req, res) => {
-  let message = req.query.message || req.body.message || 'This is a Demo index file';
-  res.status(200).send(message);
-};
+const functions = require('@google-cloud/functions-framework');
+
+functions.http('helloHttp', (req, res) => {
+ res.send(`Hello ${req.query.name || req.body.name || 'World'}!`);
+});
